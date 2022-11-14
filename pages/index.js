2cfa7bc06434c178ec1ac569dfee53da8127922c
Bao Chau <chauquocbao0907@gmail.com>
2022-11-13 21:06:24 -0800

    initial commit - learning next js
    Stop at Pre-rendering page

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your self intro]</p>
+        <p>
+          (This is a sample website)
+        </p>
+      </section>
+    </Layout>
+  );
+}
